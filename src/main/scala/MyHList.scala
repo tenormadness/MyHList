7cbe6b927c312b00ea3539a4f730ab5c041eb445
Luca Tosatto <luca.tosatto@gmail.com>
2017-10-26 00:44:53 -0400

    added comments

--- src/main/scala/MyHList.scala
+++ src/main/scala/MyHList.scala
@@ -40,4 +40,19 @@ object Test extends App {
     case head #: mid #: tail => println(s"head = $head, mid = $mid, tail= $tail")
     case _ => println("found nothing :(")
   }
+
+
+  // NOW THIS WORKS!!!
+  val baz = 1 #: "one" #: HNil
+
+  def intOnlyFunction(i: Int) = i + 1
+
+  val bazp1 = intOnlyFunction(baz.head)
+  assert(bazp1 == 2)
+
+  // NOTE THAT THIS DOES NOT WORK!
+  //
+  //val bazz = 1 :: "one" :: Nil
+  //val bazzp1 = intOnlyFunction(bazz.head)
+  //assert(bazzp1 == 2)
 }
